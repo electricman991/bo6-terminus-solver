5b63a954520549298475fa591a632db8a2705f7c
electricman991 <braidensommer@yahoo.com>
2024-11-13 20:25:59 -0800

    Added styling

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,27 @@ import './style.css'
 import { solveEquations } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
+  <div class="container">
    
     <h1>Terminus Equation Solver</h1>
 
-    <div> 
-      <label> X </label>
-      <input id="x" />
+    <div class="form-container">
+    <div class="form-group">
+      <label for="x"> X </label>
+      <input id="x" name="x" />
+    </div>
 
-      <label> Y </label>
-      <input id="y" />
+    <div class="form-group">
+      <label for="y" > Y </label>
+      <input id="y" name="y" />
+    </div>
 
-      <label> Z </label>
-      <input id="z" />
+    <div class="form-group">
+      <label for="z" > Z </label>
+      <input id="z" class="z-input" name="z" />
+    </div>
 
-      <button id="solve" type="button">Add</button>
+      <button id="solve" type="button">Solve</button>
 
       <p id="first-equation"> </p>
 
